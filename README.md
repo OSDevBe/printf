73ea886a993dc12af7c1b75ad954ccadc8533c3e
YoussefLizdihar <youssefyd.3d@gmail.com>
2023-11-13 14:31:28 -0800

    {update the main.h file}

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 0x11. C - printf
 update checker.c file
 update functions.c file
+create the functions2.c file and Update the 0-printf.c file
--- main.h
+++ main.h
@@ -23,9 +23,12 @@ typedef struct ffunc func_t;
 int _putchar(char c);
 int fchecker(const char *format, func_t f_list[], va_list ap);
 int _printf(const char *format, ...);
-int print_char(va_list list);
-int print_string(va_list list);
+int print_char(va_list ap);
+int print_string(va_list ap);
 int print_success(void);
 int print_percent(void);
+int print_integer(va_list ap);
+int print_uint(va_list ap);
 
 #endif /* _MAIN_H_ */
+
